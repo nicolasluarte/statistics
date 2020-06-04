dee1562fabacebf97a12d592e18bd96e5782ed26
nicolasluarte <l.luarte@uc.cl>
2020-06-04 05:13:33 -0400

    Added pdf with instructions on montecarlo simulation added exploratory analysis for new frequency bands ended with some logistic regressions

--- Ismael/exploratory_analysis.R
+++ Ismael/exploratory_analysis.R
@@ -1,5 +1,5 @@
 ## Load libraries
-pacman::p_load(ggplot2, purrr, tidyverse, reshape2, caret, pROC, e1071, lmtest)
+pacman::p_load(ggplot2, purrr, tidyverse, reshape2, caret, pROC, e1071, lmtest, car, furrr)
 
 ## Load data in tibble
 d <- read_csv('/home/nicoluarte/repos/statistics/Ismael/data/trial_data.csv')
@@ -99,3 +99,130 @@ calc_class_err(actual = d$anx, predicted = mdl.0_pred)
 
 ## we see almost chance
 
+## load complete data
+complete_data <- read_csv('/home/nicoluarte/repos/statistics/Ismael/data/complete_data.csv')
+
+# fix column names
+names(complete_data) <- c("type", "beta", "gamma","alpha", "anx", "n")
+colnames(complete_data)
+
+# melt data
+complete_data_melted <- melt(complete_data,
+                             id.vars = "type",
+                             measure.vars = c("beta", "gamma", "alpha"))
+head(complete_data_melted)
+# get medians
+medians <- as.data.frame(dcast( complete_data_melted, variable ~ type, median))
+
+## plot all cases + medians
+ggplot(complete_data_melted, aes(x = value, fill=as.factor(type))) +
+        facet_wrap(~variable, scales = "free_x") +
+        geom_histogram(alpha = 0.5, position = "identity") +
+        geom_vline(data = filter(complete_data_melted, variable == "beta", ),
+                   aes(xintercept = medians[1,2]), linetype = "dashed", color = "red") +
+        geom_vline(data = filter(complete_data_melted, variable == "beta", ),
+                   aes(xintercept = medians[1,3]), linetype = "dashed", color = "blue") +
+        geom_vline(data = filter(complete_data_melted, variable == "gamma", ),
+                   aes(xintercept = medians[2,2]), linetype = "dashed", color = "red") +
+        geom_vline(data = filter(complete_data_melted, variable == "gamma", ),
+                   aes(xintercept = medians[2,3]), linetype = "dashed", color = "blue") +
+        geom_vline(data = filter(complete_data_melted, variable == "alpha", ),
+                   aes(xintercept = medians[3,2]), linetype = "dashed", color = "red") +
+        geom_vline(data = filter(complete_data_melted, variable == "alpha", ),
+                   aes(xintercept = medians[3,3]), linetype = "dashed", color = "blue") 
+
+## test for normal distribution
+x <- list(0, 0, 0, 1, 1, 1)
+y <- list("beta", "gamma", "alpha",
+          "beta", "gamma", "alpha")
+# no distribution is normal
+map2(x, y, ~ complete_data %>%
+                filter(type == .x) %>% 
+                select(.y) %>% 
+                unlist()  %>% 
+                shapiro.test())
+
+# no distribution is normal plot
+qqfunc <- function(x, y) {
+        complete_data %>%
+        filter(type == x) %>% 
+        select(y) %>% 
+        unlist()  %>% 
+        qqPlot() 
+        Sys.sleep(1)
+}
+qqplots <- map2(x, y, qqfunc)
+
+## t-test
+beta.0 <- complete_data  %>% filter(anx == 1)  %>% 
+        select(beta)  %>% 
+        unlist()
+beta.1 <- complete_data  %>% filter(anx == 0)  %>% 
+        select(beta)  %>% 
+        unlist()
+gamma.0 <- complete_data  %>% filter(anx == 1)  %>% 
+        select(gamma)  %>% 
+        unlist()
+gamma.1 <- complete_data  %>% filter(anx == 0)  %>% 
+        select(gamma)  %>% 
+        unlist()
+alpha.0 <- complete_data  %>% filter(anx == 1)  %>% 
+        select(alpha)  %>% 
+        unlist()
+alpha.1 <- complete_data  %>% filter(anx == 0)  %>% 
+        select(alpha)  %>% 
+        unlist()
+
+# anxious power mean is only different for beta
+print("T-TEST")
+t.test(beta.0, beta.1)
+t.test(gamma.0, gamma.1)
+t.test(alpha.0, alpha.1)
+
+# tails are not fat and data set is relatively large
+# so t-test should be ok, however here we test with permutations
+t_test_perm <- function(x, y, sample_size, n_times) {
+        mnt_carlo <- replicate(n = n_times, expr = {
+                sample_x <- sample(x, size = sample_size, replace = FALSE)
+                sample_y <- sample(y, size = sample_size, replace = FALSE)
+                t_stat_pval <- t.test(sample_x, sample_y)$p.value
+                bool <- t_stat_pval > 0.05
+                })
+        return((length(mnt_carlo[mnt_carlo == "TRUE"]) + 1) / (n_times + 1))
+}
+
+# use all cores!
+# gamma and alpha doesnt converge
+# t-test indicates that H0 is accepted
+future::plan(multiprocess)
+beta_mnt_carlo <- seq(from = 10, to = 2000, by = 5) %>% 
+        future_map(~ t_test_perm(beta.0, beta.1, .x, 10000))
+gamma_mnt_carlo <- seq(from = 10, to = 2000, by = 5) %>% 
+        future_map(~ t_test_perm(gamma.0, gamma.1, .x, 10000))
+alpha_mnt_carlo <- seq(from = 10, to = 2000, by = 5) %>% 
+        future_map(~ t_test_perm(alpha.0, alpha.1, .x, 10000))
+
+# exploratory logistic regression
+mdl_null <- glm(anx ~ 1,
+                data = complete_data,
+                family = binomial())
+mdl_beta <- glm(anx ~ beta,
+                data = complete_data,
+                family = binomial())
+mdl_gamma <- glm(anx ~ gamma,
+                data = complete_data,
+                family = binomial())
+mdl_alpha <- glm(anx ~ alpha,
+                data = complete_data,
+                family = binomial())
+mdl_beta_gamma <- glm(anx ~ beta + gamma,
+                data = complete_data,
+                family = binomial())
+mdl_beta_gamma_alpha <- glm(anx ~ beta + gamma + alpha,
+                data = complete_data,
+                family = binomial())
+
+# likelihood ratio test
+lrtest(mdl_null, mdl_beta)
+lrtest(mdl_null, mdl_gamma)
+lrtest(mdl_null, mdl_alpha)
